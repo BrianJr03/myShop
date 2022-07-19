dafc09c2a17926a876624ead77a6f13d1b2e0f09
Brian Walker Jr <thaballa79@gmail.com>
2022-07-18 20:51:29 -0500

    finishes product detail screen

--- app/src/main/java/jr/brian/myShop/view/activities/ProductDetailActivity.kt
+++ app/src/main/java/jr/brian/myShop/view/activities/ProductDetailActivity.kt
@@ -3,6 +3,7 @@ package jr.brian.myShop.view.activities
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import jr.brian.myShop.databinding.ActivityDetailsBinding
 import jr.brian.myShop.model.remote.Constant.PRODUCT_ITEM_KEY
@@ -30,6 +31,7 @@ class ProductDetailActivity : AppCompatActivity(), DetailsMVP.DetailsView {
         supportActionBar?.hide()
         init()
         binding.back.setOnClickListener { super.onBackPressed() }
+
     }
 
 
@@ -38,6 +40,7 @@ class ProductDetailActivity : AppCompatActivity(), DetailsMVP.DetailsView {
         presenter = DetailsPresenter(VolleyHelper(this), this)
         presenter.getProductDetails(productItem.product_id)
         setAdapter(userReviews)
+        initAddToCartBtn()
     }
 
     private fun setAdapter(list: ArrayList<Review>) {
@@ -45,6 +48,58 @@ class ProductDetailActivity : AppCompatActivity(), DetailsMVP.DetailsView {
         binding.recyclerViewProductItem.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = userReviewAdapter
+        }
+    }
+
+    private fun initMainCard(detail: Detail) {
+        binding.apply {
+//            Glide.with(this@ProductDetailActivity)
+//                .load(BASE_IMAGE_URL + productItem.product_image_url)
+//                .into(productImage)
+            productDescr.text = detail.product.description
+            productName.text = detail.product.product_name
+            productPrice.text = detail.product.price
+            productRating.rating = productItem.average_rating.toFloat()
+        }
+    }
+
+    private fun initSpecsCard(detail: Detail) {
+        binding.apply {
+            modelNameSpecs.text = detail.product.product_name
+        }
+    }
+
+    private fun initAddToCartBtn() {
+        binding.apply {
+            productAddToCart.setOnClickListener {
+                productAddToCart.visibility = View.GONE
+                qtyGroup.visibility = View.VISIBLE
+                if (qtyGroup.isVisible) {
+                    incQtyBtn.setOnClickListener {
+                        var qty = productItem.qty
+                        val price = productItem.price.toInt()
+                        qty++
+                        qtyTv.text = qty.toString()
+                        productItem.qty = qty
+                        productItem.price = (qty * price).toString()
+                    }
+                    decQtyBtn.setOnClickListener {
+                        var qty = productItem.qty
+                        val price = productItem.price.toInt()
+                        qty--
+                        if (qty < 1) {
+                            qty = 1
+                            productItem.qty = 1
+                            productItem.price = price.toString()
+                            productAddToCart.visibility = View.VISIBLE
+                            qtyGroup.visibility = View.GONE
+                        }
+                        qtyTv.text = qty.toString()
+                        productItem.qty = qty
+                        productItem.price = (qty * price).toString()
+                    }
+                }
+            }
         }
     }
 
@@ -57,13 +112,25 @@ class ProductDetailActivity : AppCompatActivity(), DetailsMVP.DetailsView {
                 binding.reviewErrorGroup.visibility = View.VISIBLE
                 setAdapter(ArrayList())
             }
+            initMainCard(detail)
+            initSpecsCard(detail)
         }
     }
 
     override fun onLoad(isLoading: Boolean) {
-//        val animationView = binding.animationView
-//        if (!isLoading) {
-//            animationView.visibility = View.GONE
-//        }
+        val animationView = binding.animationView
+        binding.apply {
+            cardViewMain.visibility = View.GONE
+            cardViewDetail.visibility = View.GONE
+            detailsTv.visibility = View.GONE
+        }
+        if (!isLoading) {
+            animationView.visibility = View.GONE
+            binding.apply {
+                cardViewMain.visibility = View.VISIBLE
+                cardViewDetail.visibility = View.VISIBLE
+                detailsTv.visibility = View.VISIBLE
+            }
+        }
     }
 }
