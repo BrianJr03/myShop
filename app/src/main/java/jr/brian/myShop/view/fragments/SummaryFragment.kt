9c2218e538a25837b159540254ddfb3fd66aeb0c
Brian Walker Jr <thaballa79@gmail.com>
2022-07-19 14:54:08 -0500

    adds order confirmed activity

--- app/src/main/java/jr/brian/myShop/view/fragments/SummaryFragment.kt
+++ app/src/main/java/jr/brian/myShop/view/fragments/SummaryFragment.kt
@@ -1,17 +1,38 @@
 package jr.brian.myShop.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import jr.brian.myShop.R
+import androidx.fragment.app.Fragment
+import jr.brian.myShop.databinding.FragmentSummaryBinding
+import jr.brian.myShop.view.activities.OrderConfirmedActivity
 
 class SummaryFragment : Fragment() {
+    private lateinit var binding: FragmentSummaryBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_summary, container, false)
+    ): View {
+        binding = FragmentSummaryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            placeOrderBtn.setOnClickListener {
+                startActivity(
+                    Intent(requireContext(), OrderConfirmedActivity::class.java)
+                )
+                activity?.finish()
+            }
+        }
     }
 }
